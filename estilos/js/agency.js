b59213ba253a948c4fdfd37d744ff42e6924dc54
ZaidaLT01 <147778430+ZaidaLT01@users.noreply.github.com>
2023-11-09 19:57:54 -0400

    Correcciones del cuerpo
    Se realizaron todas las modificaciones

--- estilos/js/agency.js
+++ estilos/js/agency.js
@@ -48,3 +48,17 @@
   })
 
 })(jQuery); // End of use strict
+
+
+$('section.awSlider .carousel').carousel({
+  pause: "hover",
+  interval: 2000
+});
+
+var startImage = $('section.awSlider .item.active > img').attr('src');
+$('section.awSlider').append('<img src="' + startImage + '">');
+
+$('section.awSlider .carousel').on('slid.bs.carousel', function () {
+ var bscn = $(this).find('.item.active > img').attr('src');
+  $('section.awSlider > img').attr('src',bscn);
+});
